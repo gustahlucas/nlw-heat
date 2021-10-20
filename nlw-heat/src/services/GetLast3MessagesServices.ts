abc414cc42543f0cfef584b70719ce0bfb1b6cbc
gustahlucas <gustavo.lucas@aluno.ufop.edu.br>
2021-10-20 19:51:56 -0300

    add: aula 2

---
+++ nlw-heat/src/services/GetLast3MessagesServices.ts
@@ -0,0 +1,19 @@
+import prismaClient from "../prisma";
+
+class GetLast3MessagesServices {
+  async execute (){
+    const messages = await prismaClient.message.findMany({
+      take: 3, 
+      orderBy: {
+        create_at: "desc",
+      },
+      include: {
+        user: true, 
+      }
+    }); 
+    // SELECT * FROM MESSAGES LIMIT 3 ORDER VY CREATED_AT DESC
+    return messages; 
+  }
+}
+
+export { GetLast3MessagesServices }
